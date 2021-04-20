041a0daca847fc942c7adbf8c4a3f170e9ece1dd
mdroidhd <79988085+mdroidhd@users.noreply.github.com>
2021-04-20 17:52:12 +0200

    0.1

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function tom(){
+
+    document.getElementById("titulo").innerHTML="tom";
+    document.getElementById("enlace").src="https://pelisflix.io/?trembed=0&trid=7732&trtype=1";
+}
+
+function aladin(){
+
+    document.getElementById("titulo").innerHTML="Aladin";
+    document.getElementById("enlace").src="https://www.fembed.com/v/kxy77i35p5dj5nd";
+}
+function Falcon(){
+
+    document.getElementById("titulo").innerHTML="Falcon";
+    document.getElementById("enlace").src="https://www.fembed.com/v/xp4yys5kgq827x2";
+}
+function Fantasticos(){
+
+    document.getElementById("titulo").innerHTML="Fatasticos";
+    document.getElementById("enlace").src="https://www.fembed.com/v/0d5wwfl7j7608k4";
+};
